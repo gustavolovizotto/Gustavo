4be54bf462cae0aa97d933376172a90d4ca0e59f
Renato Lovizotto <lovizotto@gmail.com>
2023-01-27 16:49:39 -0300

    Added prettier and node to project

--- calculadora/java.js
+++ calculadora/java.js
@@ -1,26 +1,24 @@
-const resultadoElement = document.getElementById('resultado');
+const resultadoElement = document.getElementById("resultado")
 console.log(resultadoElement)
-function insert(num)
-    {
-     var numero =   resultadoElement.innerHTML;
-    resultadoElement.innerHTML = numero + num;
-    }
-    function clean() {
-        resultadoElement.innerHTML = "";
-    }
+function insert(num) {
+  var numero = resultadoElement.innerHTML
+  resultadoElement.innerHTML = numero + num
+}
+function clean() {
+  resultadoElement.innerHTML = ""
+}
 
-    function back() {
-        var resultado = resultadoElement.innerHTML;
-        resultadoElement.innerHTML = resultado.substring(0, resultado.length -1);
-    }
+function back() {
+  var resultado = resultadoElement.innerHTML
+  resultadoElement.innerHTML = resultado.substring(0, resultado.length - 1)
+}
 
-    function calc() {
-       var resultado = resultadoElement.innerHTML;
-        if(resultado) {
-            var resultado = resultadoElement.innerHTML;
-            resultadoElement.innerHTML = eval(resultado);
-        }
-        else {
-            resultadoElement.innerHTML = "nada para calcular"
-        }
-    }
+function calc() {
+  var resultado = resultadoElement.innerHTML
+  if (resultado) {
+    var resultado = resultadoElement.innerHTML
+    resultadoElement.innerHTML = eval(resultado)
+  } else {
+    resultadoElement.innerHTML = "nada para calcular"
+  }
+}
