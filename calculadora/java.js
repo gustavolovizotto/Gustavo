6fca65a40f2ddc42a84e3b11a9f39786f89a7d72
gustavolovizotto <gustavolovizotto@gmail.com>
2023-01-27 16:34:00 -0300

    First commit

---
+++ calculadora/java.js
@@ -0,0 +1,26 @@
+const resultadoElement = document.getElementById('resultado');
+console.log(resultadoElement)
+function insert(num)
+    {
+     var numero =   resultadoElement.innerHTML;
+    resultadoElement.innerHTML = numero + num;
+    }
+    function clean() {
+        resultadoElement.innerHTML = "";
+    }
+
+    function back() {
+        var resultado = resultadoElement.innerHTML;
+        resultadoElement.innerHTML = resultado.substring(0, resultado.length -1);
+    }
+
+    function calc() {
+       var resultado = resultadoElement.innerHTML;
+        if(resultado) {
+            var resultado = resultadoElement.innerHTML;
+            resultadoElement.innerHTML = eval(resultado);
+        }
+        else {
+            resultadoElement.innerHTML = "nada para calcular"
+        }
+    }
